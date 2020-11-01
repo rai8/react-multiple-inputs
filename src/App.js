588427ff83c6329fe47feeb7d7d5133cba2f67dd
rai8 <hytonne@outlook.com>
2020-11-01 15:46:25 +0300

    handling react multiple forms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import './App.css'
 
 function App() {
+  const [fname, setFname] = useState()
+  const [lname, setLname] = useState()
+  const { register, handleSubmit, errors } = useForm()
+  const onSubmit = ({ fname, lname }) => {
+    //console.log(data)
+    setFname(fname)
+    setLname(lname)
+  }
+  const handleChange = e => {
+    e.preventDefault()
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <div className='container field'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className='form-group'>
+            <input
+              type='text'
+              id='fname'
+              name='fname'
+              placeholder='First Name'
+              onChange={handleChange}
+              ref={register({ required: true })}
+            />
+            {errors.fname && errors.fname.type === 'required' && <span>This is required</span>}
+          </div>
+          <div className='form-group'>
+            <input
+              type='text'
+              id='lname'
+              name='lname'
+              placeholder='Last Name'
+              onChange={handleChange}
+              ref={register({ required: true })}
+            />
+            {errors.lname && errors.lname.type === 'required' && <span>This is required</span>}
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-primary btn-block'>CLICK ME !!!</button>
+          </div>
+        </form>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My name is {fname} {lname}{' '}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
